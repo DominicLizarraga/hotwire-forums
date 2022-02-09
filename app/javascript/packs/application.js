215a2c41cf3da19b741268a0ae702d37721c4504
dominic lizarraga <dominiclizarraga@hotmail.com>
2022-02-08 21:44:28 -0800

    adding hotwire

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,15 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+import "./controllers"
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+import turbo from "turbo"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "@hotwired/turbo-rails"
 
 Rails.start()
-Turbolinks.start()
+turbo.start()
 ActiveStorage.start()
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
